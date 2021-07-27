fcc4437fb34af2414ad3fc5f57057d8000bbc870
Rainbow Dreamer <59408627+Rainbow-Dreamer@users.noreply.github.com>
2021-07-28 01:25:57 +0800

    update

--- matrixpro editor/config.py
+++ matrixpro editor/config.py
@@ -1,2 +1,28 @@
-﻿config_dict = {'background_image': 'bg.jpg', 'background_places': (700, 0), 'eachline_character': 85, 'pairing_symbols': ['()', '[]', "''", '""'], 'wraplines_number': 2}
-path_enable_list = ['background_image']
+﻿config_dict = {
+    'background_image': 'bg.jpg',
+    'background_places': (840, -30),
+    'eachline_character': 58,
+    'pairing_symbols': ['()', '[]', "''", '""'],
+    'wraplines_number': 2,
+    'font_type': 'Consolas',
+    'font_size': 15,
+    'background_mode': 'white',
+    'grammar_highlight': {
+        'red': ['/', '@', '^', '~', '%', '&', '|', '+', '-', '?', '*'],
+        'blue': ['getchord', 'chord', 'trans', 'chd', 'C'],
+        'green': ['toScale', 'scale', 'S'],
+        'orange': ['toNote', 'note', 'N'],
+        'violet': ['piece', 'build', 'P']
+    },
+    'background_color': 'white',
+    'foreground_color': 'black',
+    'active_background_color': 'ivory2',
+    'day_and_night_colors': ('white', 'gray8'),
+    'search_highlight_color': ['light blue', 'light green'],
+    'button_background_color': 'light grey',
+    'active_foreground_color': 'black',
+    'disabled_foreground_color': 'white',
+    'highlight_background': 'ivory2',
+    'highlight_color': 'ivory2',
+    'highlight_thickness': 2
+}
